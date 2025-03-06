27f43e87b9b7ec3ad07c79c802b53b7308d7afe6
Allan <allanbarongo5@gmail.com>
2025-03-06 12:40:44 +0300

    completed fuctions lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+let blocksAway
+let feetAway
+let trip
+let fare
+let pickUp 
+function distanceFromHqInBlocks(pickUp){
+    blocksAway=Math.abs(pickUp-42)
+    return blocksAway
+}
+
+function distanceFromHqInFeet(pickUp){
+      feetAway=Math.abs(pickUp-42)*264
+    return feetAway
+}
+
+function distanceTravelledInFeet(start, destination){
+    trip=Math.abs((start-destination)*264)
+    return trip
+}
+
+function calculatesFarePrice(start, destination){
+    trip=Math.abs((start-destination)*264)
+    
+    if(trip<=400){
+        fare=0
+    }
+    else if(trip>400 && trip<=2000){
+        fare=(trip-400)*(2/100)
+    }
+    else if(trip>2000 & trip<=2500){
+        fare=25
+    }
+    else{
+        fare= 'cannot travel that far'
+    }
+    return fare
+}
+
